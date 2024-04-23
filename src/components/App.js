3f740a5470d421a57767d1aae91fcfb6be33a5b9
Joel John <johnjoel079@gmail.com>
2024-04-23 08:15:20 -0400

    Lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+function About(){ 
+  return ( 
+    <div id = "about"> 
+    <h2>About</h2>
+    </div>
+  )
+} 
+
+function NavBar(){ 
+  return ( 
+    <div id = "navbar"> 
+    <h2>NavBar</h2>
+    </div>
+  )
+} 
+
+function Home(){ 
+  return ( 
+    <div id = "home"> 
+    <h2>Home</h2>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
